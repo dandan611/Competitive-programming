bd2525230be4a9c571a426a122cd9972db5f3205
dandan611 <kohonen_rnn@hotmail.co.jp>
2020-06-08 23:33:41 +0900

    learning java bronze

---
+++ Java/java se bronze/2-14/ArrayTest.java
@@ -0,0 +1,10 @@
+class ArrayTest{
+    public static void main(String args[]){
+        int i[] = new int[3];
+        char c[] = new char[2];
+        String s[] = new String[3];
+        System.out.print(i[0]+":");
+        System.out.print(c[1]+":");
+        System.out.print(s[2]+":");
+    }
+}
