c3844c567dca3f12f59ff0de5bfab0dd04878054
dandan611 <kohonen_rnn@hotmail.co.jp>
2020-06-08 23:22:43 +0900

    0607 result

---
+++ Java/java se bronze/2-13/ArrayTest.java
@@ -0,0 +1,10 @@
+class ArrayTest{
+    public static void main(String args[]){
+        String[] str = {"AAA","BBB","CCC"};
+        char ch[] ={'A','B'};
+        int[] i = {100,200,300};
+        System.out.print(str[1]+":");
+        System.out.print(ch[2]+":");
+        System.out.print(i[3]);
+    }
+}
