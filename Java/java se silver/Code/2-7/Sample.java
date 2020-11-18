1f9708f8500aba7961cf3119956bb0225019458c
dandan611 <kohonen_rnn@hotmail.co.jp>
2020-11-18 17:18:59 +0900

    learning java silver

---
+++ Java/java se silver/Code/2-7/Sample.java
@@ -0,0 +1,9 @@
+public class Sample{
+    private var value;
+    public Sample(var value){
+        this.value = value;
+    }
+    public void test(){
+        System.out.println(value);
+    }
+}
