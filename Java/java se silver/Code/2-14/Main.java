6bfb88f5d2100fc469c1ec41d5e523991cf89fc0
dandan611 <kohonen_rnn@hotmail.co.jp>
2020-11-18 18:45:26 +0900

    learning java silver section 2

---
+++ Java/java se silver/Code/2-14/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args){
+        String str = "aaaa";
+        System.out.println(str.replace("aa","b"));
+    }
+}
---
+++ Java/java se silver/Code/2-21/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder("abcde");
+        sb.reverse();
+        sb.replace(1,3,"a");
+        System.out.println(sb);
+    }
+}
